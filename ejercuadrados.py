bdd73b247eee40d0136c4787ed406a3655569e72
samu <samuel.alvarez@iesdonana.org>
2024-12-05 19:30:42 +0100

    Add files via upload

--- ejercuadrados.py
+++ ejercuadrados.py
@@ -16,8 +16,8 @@ cuadrado(1)=
   ]  = [[1]]
 '''
 
-def cuadrado(n: int) -> list(list[int]):
-    """Devuelve un cuadrado de n * n con los números del 1 al n. """
+"""def cuadrado(n: int) -> list(list[int]):
+    Devuelve un cuadrado de n * n con los números del 1 al n.
     res = []
     c = 1
     for i in range(n):
@@ -27,6 +27,17 @@ def cuadrado(n: int) -> list(list[int]):
             c += 1
         res.append(f)
     return res
+    
+        
+    
+def imprimir_cuadrado(c: list[list[int]]) -> None:
+    Imprime el cuadrado generado por la función cuadrado().
+    for fila in c:
+        for e in fila:
+            print(f"{e:3}", end=' ')
+        print()"""
+
+from itertools import count
 
 def imprimir_cuadrado(c: list[list[int]]) -> None:
     """Imprime el cuadrado generado por la función cuadrado()."""
@@ -34,9 +45,23 @@ def imprimir_cuadrado(c: list[list[int]]) -> None:
         for e in fila:
             print(f"{e:3}", end=' ')
         print()
-        
-    
-    
-imprimir_cuadrado(cuadrado(10))
-    
 
+def magico (n:int) -> list[list[int]]:    
+    """Devuelve el cuadrado mágico de n * n."""
+    res = [[0] * n for _ in range(n)]
+    c = count(1)
+    fila, col = 0, n // 2
+    for _ in range (n * n):
+        res[fila][col] = next(c)
+        # nueva_fila = n - 1 if fila == 0 else fila - 1
+        # nueva_col = 0 if col == n - 1 else col + 1
+        nueva_fila, nueva_col = (fila - 1) % n, (col + 1) % n
+        if res[nueva_fila][nueva_col] != 0:
+            nueva_fila, nueva_col = fila + 1, col
+        fila, col = nueva_fila, nueva_col
+    return res
+
+def comprobar_magico(c: list[list[int]]) -> bool:
+    """Comprueba si un cuadrado es mágico."""
+    
+imprimir_cuadrado(magico(5))
